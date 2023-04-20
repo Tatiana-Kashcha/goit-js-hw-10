7f0de127bcabaa5ba175f5f1a74f3a5a2f93716a
Tatiana Kashcha <tkashcha@gmail.com>
2023-04-20 20:58:28 +0300

    додає fetchCountries

---
+++ src/fetchCountries.js
@@ -0,0 +1,12 @@
+function fetchCountries(name) {
+  const url = `https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags,languages`;
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  });
+}
+export { fetchCountries };
+
+//?fields={name.official};{capital};{population};{flags.svg};{languages}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+import debounce from 'lodash.debounce';
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+const searchEl = document.querySelector('#search-box');
+
+searchEl.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(evt) {
+  const country = evt.target.value;
+
+  fetchCountries(country)
+    .then('ok')
+    .catch(error => console.log(error))
+    .finally(() => {
+      //   searchEl.value = '';
+    });
+}
